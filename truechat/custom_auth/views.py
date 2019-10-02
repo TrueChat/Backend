dde44773295f837552fd509d5d634537535fc887
gda2048 <goncharovdma@gmail.com>
2019-10-02 23:34:01 +0300

    add profile

--- truechat/custom_auth/views.py
+++ truechat/custom_auth/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from rest_framework import generics, permissions
+from custom_auth.serializers import UserSerializer
 
-# Create your views here.
+User = get_user_model()
+
+
+class UserIsOwnerOrReadOnly(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return obj.id == request.user.id
+
+
+class UserAPIView(generics.RetrieveUpdateAPIView):
+    queryset = User.objects.all()
+    permission_classes = (
+        permissions.IsAuthenticated,
+        UserIsOwnerOrReadOnly,
+    )
+    serializer_class = UserSerializer
+
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
--- truechat/truechat/urls.py
+++ truechat/truechat/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import url, include
+from django.urls import path
 from rest_framework_swagger.views import get_swagger_view
+from custom_auth.views import UserAPIView
 
 schema_view = get_swagger_view(title='TrueChat API')
 
@@ -7,4 +9,5 @@ urlpatterns = [
     url(r'^$', schema_view),
     url(r'^rest-auth/', include('rest_auth.urls')),
     url(r'^rest-auth/registration/', include('rest_auth.registration.urls')),
+    path('profile/<pk>/', UserAPIView.as_view()),
 ]
