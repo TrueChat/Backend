f5e34e3c3ed1d36810ea36c67b70930868f78ede
Dmytro Honcharov <goncharovdma@gmail.com>
2019-10-15 18:43:01 +0300

    fix admin

--- truechat/chat/admin.py
+++ truechat/chat/admin.py
@@ -1,5 +1,18 @@
 from django.contrib import admin
 from .models import Message, Chat
 
-admin.site.register(Chat)
+
+class MembershipInline(admin.StackedInline):
+    model = Chat.users.through
+    fields = ('user', 'is_admin')
+    extra = 1
+
+
+@admin.register(Chat)
+class ChatAdmin(admin.ModelAdmin):
+    fields = ('name', 'description', 'is_dialog', 'notifications')
+    list_display = ('name', 'is_dialog')
+    inlines = [MembershipInline, ]
+
+
 admin.site.register(Message)
