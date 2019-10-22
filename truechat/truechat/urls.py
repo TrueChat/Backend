727144fd8f1cbb842fa9d01a1c56fd7f5a9fdb7f
Dmytro Honcharov <goncharovdma@gmail.com>
2019-10-22 13:30:54 +0300

    add reset view

--- truechat/truechat/urls.py
+++ truechat/truechat/urls.py
@@ -10,12 +10,17 @@ schema_view = get_swagger_view(title='TrueChat API')
 urlpatterns = [
     url(r'^$', schema_view, name='api'),
     url(r'^rest-auth/', include('rest_auth.urls')),
+
+
+    url(r'^reset/(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$', confirm_email, name='password_reset_confirm'),
+    # after registration user is sent to this url for email confirming
     url(r'^rest-auth/registration/account-confirm-email/(?P<key>[-:\w]+)/$',
         confirm_email,
         name='account_confirm_email'),
     url(r'^rest-auth/registration/account-confirm-email/',
         confirm_email,
         name='account_email_verification_sent'),
+
     url(r'^rest-auth/registration/', include('rest_auth.registration.urls')),
     re_path(r'profile/(?P<username>\w+|)', UserAPIViewChange.as_view()),
     path('admin/', admin.site.urls)
