11bbf77567e9fdc6a8ff7abbdcaf24b352e50a05
Dmytro Honcharov <goncharovdma@gmail.com>
2019-10-23 18:48:14 +0300

    add raw ChatAPI_View

--- truechat/chat/views.py
+++ truechat/chat/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import permissions
 
-# Create your views here.
+
+class ChatAPI(APIView):
+    permission_classes = [permissions.IsAuthenticated]
