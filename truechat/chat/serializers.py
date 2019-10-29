2be2784cf176ed0d204947367cb40cd71e34f768
Dmytro Honcharov <goncharovdma@gmail.com>
2019-10-29 16:56:57 +0200

    return only unbanned users in chat

--- truechat/chat/serializers.py
+++ truechat/chat/serializers.py
@@ -1,12 +1,19 @@
 from chat.models import Chat, Message
 from rest_framework import serializers
 from custom_auth.serializers import UserSerializerGet
+from custom_auth.models import User
 
 
 class ChatSerializer(serializers.ModelSerializer):
     """Chat room serialization"""
     creator = UserSerializerGet()
-    users = UserSerializerGet(many=True)
+    users = serializers.SerializerMethodField()
+
+    def get_users(self, instance):
+        users = []
+        for member in instance.members.filter(is_banned=False):
+            users.append(member.user)
+        return UserSerializerGet(users, many=True).data
 
     class Meta:
         model = Chat
