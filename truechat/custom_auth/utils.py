f768dfc4e03efcc4a379ee1107cbb5e479901afd
Dmytro Honcharov <goncharovdma@gmail.com>
2019-11-01 14:59:11 +0200

    fix utils

--- truechat/custom_auth/utils.py
+++ truechat/custom_auth/utils.py
@@ -1,7 +1,18 @@
-from custom_auth.models import User
 from allauth.account.models import EmailAddress
+
+from custom_auth.models import User
+
+
+def _set_password(user):
+    user.set_password(user.password)
+    user.save()
 
 
 def confirm_emails():
     emails = [el.email for el in EmailAddress.objects.all()]
-    [EmailAddress(user=el, email=el.email, verified=True, primary=True) for el in User.objects.all() if el.email not in emails]
+    [EmailAddress(user=el, email=el.email, verified=True, primary=True).save() for el in User.objects.all() if
+     el.email not in emails]
+
+
+def confirm_passwords():
+    [_set_password(user) for user in User.objects.all() if user.password[:5] != 'pbkdf']
