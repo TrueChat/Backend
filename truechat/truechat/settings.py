932ea04773fecc6b8defd30e0107685f3037bf67
gda2048 <goncharovdma@gmail.com>
2019-11-29 01:02:25 +0200

    fix cdn

--- truechat/truechat/settings.py
+++ truechat/truechat/settings.py
@@ -68,7 +68,9 @@ else:
 cloudinary.config(
     cloud_name=CLOUDINARY_CLOUD_NAME,
     api_key=CLOUDINARY_API_KEY,
-    api_secret=CLOUDINARY_API_SECRET
+    api_secret=CLOUDINARY_API_SECRET,
+    private_cdn=False,
+    secure=True
 )
 
 SITE_ID = 1
